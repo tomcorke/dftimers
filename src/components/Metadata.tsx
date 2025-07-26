3afc30c6a57012223937353e74f5aaec8f3a2a3e
Tom Corke <tom@tomcorke.com>
2025-07-26 01:57:13 +0100

    color

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -2,7 +2,7 @@ import metadata from '../data/build-metadata.json';
 
 export const Metadata = () => (
   <div className="metadata">
-    <div className="git-sha">{metadata.commitSha.slice(0, 6)}</div>
+    <div className="git-sha" style={{ color: `#${metadata.commitSha.slice(0, 6)}` }}>{metadata.commitSha.slice(0, 6)}</div>
     <div className="git-date">{metadata.commitDate}</div>
   </div>
 );
